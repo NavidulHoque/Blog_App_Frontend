fc1f471940d69da632220a48dd247ef22f9104d8
navidul hoque <hnavidul@gmail.com>
2024-10-08 14:26:19 +0600

    frontend

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,7 @@
 import io from 'socket.io-client';
 import { url } from './url';
 
-const socket = io.connect(url);
+const socket = io.connect(url, {
+    withCredentials: true,
+});
 export default socket;
